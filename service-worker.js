1595ae82c7b794a36d74caef40f2e6f0949a0e56
Geison Souza <geisonpixpicpay@gmail.com>
2026-08-04 17:31:02 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
-const CACHE='foto-turismo-v241-root-images';
-const ASSETS=['./','./index.html','./styles.css?v=241','./app.js?v=241','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./praia-do-forte-3d-bg.png','./praia-do-forte-3d-suave-front.png','./praia-do-forte-3d-bold-front.png','./praia-do-forte-3d-suave-thumb.png','./praia-do-forte-3d-bold-thumb.png'];
+const CACHE='foto-turismo-v25';
+const ASSETS=[
+  './','./index.html','./styles.css','./app.js','./manifest.json',
+  './icons/icon-192.png','./icons/icon-512.png',
+  './praia-do-forte-3d-bg.png','./praia-do-forte-3d-suave-front.png','./praia-do-forte-3d-bold-front.png',
+  './praia-do-forte-3d-suave-thumb.png','./praia-do-forte-3d-bold-thumb.png'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return r;}).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
